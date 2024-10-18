2c6a6c403f264ae393963b7073d25ae854d96740
Jakob Helgesson <jakob.helgesson@gmail.com>
2024-10-18 12:08:15 +0200

    bump

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,6 +38,7 @@ export default function Home() {
                         .
                     </li>
                     <li>Save and see your changes instantly.</li>
+                    <li>{backendUrl}</li>
                 </ol>
 
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
